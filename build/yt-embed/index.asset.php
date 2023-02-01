4b124889032e56167e92923a597db050bbeea4af
pattyok <patty.ohara@gmail.com>
2023-02-01 11:33:43 -0800

    adding options

--- build/yt-embed/index.asset.php
+++ build/yt-embed/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '7154348f26d6a897af29');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '21a2b59f04f58d12b271');
